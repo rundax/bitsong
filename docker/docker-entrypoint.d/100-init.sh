aa2eb7872644b030195a6efe22938fce22f9716b
Andru Cherny <a.cherny@adgoal.net>
2021-03-23 14:02:44 +0200

    .

--- docker/docker-entrypoint.d/100-init.sh
+++ docker/docker-entrypoint.d/100-init.sh
@@ -5,7 +5,7 @@ set -u
 set -o pipefail
 
 
-if [ ! -d /root/.bitsongd ]; then
+if [ ! -d /home/bitsong/.bitsongd/ ]; then
     bitsongd init --chain-id ${BITSONG_INIT_CHAIN_ID} ${BITSONG_INIT_MONIKER}
 #    bitsongcli keys add --chain-id ${BITSONG_INIT_CHAIN_ID} ${BITSONG_INIT_WALLET_NAME}
     wget https://raw.githubusercontent.com/bitsongofficial/networks/master/${BITSONG_INIT_CHAIN_ID}/genesis.json -o /home/bitsong/.bitsongd/config/genesis.json
