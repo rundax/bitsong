63d6043b51ff73d776e8ab885a635f56c16b6b2f
Andru Cherny <a.cherny@adgoal.net>
2021-03-23 14:08:12 +0200

    Add creation directory

--- docker/docker-entrypoint.d/100-init.sh
+++ docker/docker-entrypoint.d/100-init.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 
-if [ ! -d /home/bitsong/.bitsongd/config ]; then
+if [ ! -d /home/bitsong/.bitsongd/config  ]; then
     bitsongd init --chain-id ${BITSONG_INIT_CHAIN_ID} ${BITSONG_INIT_MONIKER}
 #    bitsongcli keys add --chain-id ${BITSONG_INIT_CHAIN_ID} ${BITSONG_INIT_WALLET_NAME}
-    wget https://raw.githubusercontent.com/bitsongofficial/networks/master/${BITSONG_INIT_CHAIN_ID}/genesis.json -o /home/bitsong/.bitsongd/config/genesis.json
+    wget --force-directories https://raw.githubusercontent.com/bitsongofficial/networks/master/${BITSONG_INIT_CHAIN_ID}/genesis.json -o /home/bitsong/.bitsongd/config/genesis.json
 fi
